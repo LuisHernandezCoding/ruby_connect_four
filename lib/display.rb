28d1291962d57d50f913beda266f9f025c369a46
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-10-28 15:45:28 -0500

    - Added display class to handle the display of the game - Changes to game.rb to respond to the display class - Changes to the graphics.rb to include the board

---
+++ lib/display.rb
@@ -0,0 +1,16 @@
+require_relative 'colors'
+require_relative 'console_printer'
+require_relative 'graphics'
+
+class Display
+  include ConsolePrinter
+  include Graphics
+
+  def print_board(board, messages = [], size = 100)
+    system 'clear' or system 'cls'
+    screen = compound_screen(board)
+    screen += messages.map { |line| line.center(size - 26) } unless messages.empty?
+    print_message(screen, size, 'bg_dark gray', 'bg_blue', use_frame: true)
+    print_input_field(size, 'bg_black', '>')
+  end
+end
